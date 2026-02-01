b10a952761f7207152a3ee09bc174bd11e2beaf3
pwnjoshi <joshipawan2021@gmail.com>
2026-02-01 17:52:13 +0530

    Easier Version for Calculator added

---
+++ Programs_2.0/Calculator.java
@@ -0,0 +1,60 @@
+// Design a command-line calculator that can perform basic arithmetic operations (+, -, *, /) on two numbers. The program should be able to handle both integer and floating-point inputs. If a non-numeric argument is provided, it should gracefully inform the user. The program should use Wrapper Classes for internal number representation and demonstrate Autoboxing during calculations.
+
+
+class arithmetic{
+
+    static void add(Double num1, Double num2){
+        double sum = num1+num2;
+        System.out.println("Sum: " + sum);
+    }
+
+    static void subtract(Double num1, Double num2){
+        double difference = num1 - num2;
+        System.out.println("Difference: " + difference);
+    }
+    static void multiply(Double num1, Double num2){
+        double product = num1 * num2;
+        System.out.println("Product: " + product);
+    }
+    static void divide(Double num1, Double num2){
+        if(num2 != 0){
+            double quotient = num1 / num2;
+            System.out.println("Quotient: " + quotient);
+        } else {
+            System.out.println("Error: Division by zero is not allowed.");
+        }
+    }
+}
+public class Calculator {
+
+    public static void main(String[] args){
+        if(args.length != 3){
+            System.out.println("Usage: java Calculator <number1> <operator> <number2>");
+            return;
+        }
+            Double n1 = Double.parseDouble(args[0]);
+            Double n2 = Double.parseDouble(args[2]);
+            char operator = args[1].charAt(0);
+
+            switch(operator){
+                case '+' -> {
+                        
+                        arithmetic.add(n1,n2);
+                }
+
+                case '-' -> {
+                        arithmetic.subtract(n1,n2);
+                }
+
+                case '*' -> {
+                        arithmetic.multiply(n1,n2);
+                }
+
+                case '/' -> {
+                        arithmetic.divide(n1,n2);
+                }
+                default -> System.out.println("Invalid operator. Please use +, -, *, or /.");
+            }
+    }
+
+}
